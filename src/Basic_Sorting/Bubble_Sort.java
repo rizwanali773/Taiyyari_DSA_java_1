ffd1271e6b7e81be7e8cea0abca94245f81af9d7
rizwanali773 <167829500+rizwanali773@users.noreply.github.com>
2026-07-26 15:29:23 +0530

    bubble sort

---
+++ src/Basic_Sorting/Bubble_Sort.java
@@ -0,0 +1,42 @@
+package Basic_Sorting;
+
+import java.util.Scanner;
+
+public class Bubble_Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int size = sc.nextInt();
+
+        System.out.print("Enter elements of array: ");
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Sorted Array: ");
+        bubble_sort(arr);
+        print(arr);
+    }
+
+    public static void bubble_sort(int[] arr){
+        int n = arr.length - 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void print(int[] arr){
+        for (int el : arr){
+            System.out.print(el+" ");
+        }
+        System.out.println();
+    }
+}
